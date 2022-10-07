92cdb5e39a1e687b2d6d6dca672d210247cee7b2
Giovanni <giovanni.tnini@gmail.com>
2022-10-07 11:24:49 -0300

    Acresentando exceções no código e aprendendo a tratá-las

---
+++ java-pilha/src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        String nome = "Alura";
+        System.out.println("ANTES");
+
+        try {
+            System.out.println(nome);
+        } catch(ArrayIndexOutOfBoundsException ex) {
+            System.out.println("CATCH");
+        }
+
+        System.out.println("DEPOIS");
+    }
+}
